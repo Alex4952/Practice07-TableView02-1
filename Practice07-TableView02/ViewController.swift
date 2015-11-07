b2c011154e14a88997d55ee2a3c5291671f17997
Mac <Mac@macui-imac-6.local>
2015-11-07 14:07:33 +0900

    Initial Commit

---
+++ Practice07-TableView02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practice07-TableView02
+//
+//  Created by Mac on 2015. 11. 7..
+//  Copyright © 2015년 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
